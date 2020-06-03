6554ab74a0b46080fb2f9fbfe073ba5d28a239f9
wkaag-ijssel <63787662+wkaag-ijssel@users.noreply.github.com>
2020-06-03 08:30:55 +0200

    update work-performance

--- js/work-performance.js
+++ js/work-performance.js
@@ -19,6 +19,182 @@ let pMain = "#1a237e"; //'rgb(26,35,126)';
 //     }
 // });
 
+let ctx1 = document.getElementById('taskCompletion').getContext('2d');
+var myDoughnutChart = new Chart(ctx1, {
+    type: 'doughnut',
+    data: {
+        datasets: [{
+            data: [95, 5],
+            backgroundColor: [
+                pMain
+            ]
+        }],
+    
+        // These labels appear in the legend and in the tooltips when hovering different arcs
+        labels: [
+            'Work completed (%)',
+            'Work not completed (%)'
+        ]
+    },
+    options: {
+        cutoutPercentage: 70,
+        responsive: true,
+        maintainAspectRatio: false,
+        legend: {
+            display: false
+        },
+        elements: {
+            center: {
+                text: '95%',
+                fontStyle: 'Arial', // Default is Arial
+                sidePadding: 50, // Default is 20 (as a percentage)
+                minFontSize: 10, // Default is 20 (in px), set to false and text will not wrap.
+                lineHeight: 10 // Default is 25 (in px), used for when text wraps
+            }
+        }
+    }
+});
+
+Chart.pluginService.register({
+    beforeDraw: function(chart) {
+      if (chart.config.options.elements.center) {
+        // Get ctx from string
+        var ctx = chart.chart.ctx;
+
+        // Get options from the center object in options
+        var centerConfig = chart.config.options.elements.center;
+        var fontStyle = centerConfig.fontStyle || 'Arial';
+        var txt = centerConfig.text;
+        var color = centerConfig.color || '#000';
+        var maxFontSize = centerConfig.maxFontSize || 40;
+        var sidePadding = centerConfig.sidePadding || 20;
+        var sidePaddingCalculated = (sidePadding / 100) * (chart.innerRadius * 2)
+        // Start with a base font of 30px
+        ctx.font = "30px " + fontStyle;
+
+        // Get the width of the string and also the width of the element minus 10 to give it 5px side padding
+        var stringWidth = ctx.measureText(txt).width;
+        var elementWidth = (chart.innerRadius * 2) - sidePaddingCalculated;
+
+        // Find out how much the font can grow in width.
+        var widthRatio = elementWidth / stringWidth;
+        var newFontSize = Math.floor(30 * widthRatio);
+        var elementHeight = (chart.innerRadius * 2);
+
+        // Pick a new font size so it will not be larger than the height of label.
+        var fontSizeToUse = Math.min(newFontSize, elementHeight, maxFontSize);
+        var minFontSize = centerConfig.minFontSize;
+        var lineHeight = centerConfig.lineHeight || 25;
+        var wrapText = false;
+
+        if (minFontSize === undefined) {
+          minFontSize = 20;
+        }
+
+        if (minFontSize && fontSizeToUse < minFontSize) {
+          fontSizeToUse = minFontSize;
+          wrapText = true;
+        }
+
+        // Set font settings to draw it correctly.
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+        var centerX = ((chart.chartArea.left + chart.chartArea.right) / 2);
+        var centerY = ((chart.chartArea.top + chart.chartArea.bottom) / 2);
+        ctx.font = fontSizeToUse + "px " + fontStyle;
+        ctx.fillStyle = color;
+
+        if (!wrapText) {
+          ctx.fillText(txt, centerX, centerY);
+          return;
+        }
+
+        var words = txt.split(' ');
+        var line = '';
+        var lines = [];
+
+        // Break words up into multiple lines if necessary
+        for (var n = 0; n < words.length; n++) {
+          var testLine = line + words[n] + ' ';
+          var metrics = ctx.measureText(testLine);
+          var testWidth = metrics.width;
+          if (testWidth > elementWidth && n > 0) {
+            lines.push(line);
+            line = words[n] + ' ';
+          } else {
+            line = testLine;
+          }
+        }
+
+        // Move the center up depending on line height and number of lines
+        centerY -= (lines.length / 2) * lineHeight;
+
+        for (var n = 0; n < lines.length; n++) {
+          ctx.fillText(lines[n], centerX, centerY);
+          centerY += lineHeight;
+        }
+        //Draw text in center
+        ctx.fillText(line, centerX, centerY);
+      }
+    }
+});
+
+let labels = []
+let date = new Date();
+for(let i = 0; i < 50; i += 1){
+    labels[i] = [date.getDate("October 12")];
+    date = new Date(date);
+}
+
+/**Chart 2: Completed work over time*/
+let ctx = document.getElementById('workLoadChart').getContext('2d');
+let resourcesChart = new Chart(ctx, {
+    type: 'bar',
+    data: { 
+        labels: labels,
+        datasets: [{
+            label: 'Percentage of tasks completed',
+            borderColor: pDark,
+            fill: false,
+            data: Array.from({length: 50}, () => Math.random())
+        }]
+    },
+    options: {
+        legend: {
+            display: false,
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+        elements: {
+            line: {
+                tension: 0, // disables bezier curves
+                borderWidth: 1
+            },
+            point:{
+                radius: 0 //hide data point indicators
+            }
+        },
+        scales: {
+            xAxes: [{
+                gridLines: {
+                    display: false,
+                },
+                ticks: {
+                    autoSkip: true,
+                    maxRotation: 0,
+                    minRotation: 0,
+                    minorTick: {
+                        fontSize: 7
+                    }
+                }
+            }],
+            yAxes: [{
+                position: 'right'
+            }]
+        }
+    }
+});
+
 /**
  * @param {string} chartName The chart variable name
  * @param {array} data The new dataset 
@@ -203,15 +379,31 @@ let getDataPromise = new Promise((resolve) => {
     });
     resolve(scatterArray);
 }).then(data => {   
-    console.log(data);
     let ctx6 = document.getElementById('executedWorkChart').getContext('2d');
     let executedWorkChart = new Chart(ctx6, {
-        type: 'scatter',
         data: {
             datasets: [{
+                type: 'scatter',
                 label: 'Scatter Dataset',
+                showLine: false,
                 borderColor: pMain,
                 data: data
+            },{
+                type: 'line',
+                label: 'target',
+                data: [{
+                    x: 0,
+                    y: 0
+                },{
+                    x: 30,
+                    y: 30
+                }],
+                borderColor: pDark,
+                borderWidth: 1,
+                borderDash: [5,2],
+                fill: false,
+                PointStyle: 'none',
+                radius: 0
             }]
         },
         options: {
@@ -224,7 +416,7 @@ let getDataPromise = new Promise((resolve) => {
                     position: 'bottom',
                     scaleLabel: {
                         display: true,
-                        labelString: 'not executed'
+                        labelString: 'Generated'
                     }
                 }],
                 yAxes: [{
@@ -232,7 +424,7 @@ let getDataPromise = new Promise((resolve) => {
                     position: 'left',
                     scaleLabel: {
                         display: true,
-                        labelString: 'executed'
+                        labelString: 'Completed'
                     }
                 }]
             }
